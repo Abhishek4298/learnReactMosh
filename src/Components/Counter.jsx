687ac1ecdd9abc9b00ca0f4ed62b454cdfa6e987
Abhishek4298 <abhishek.bhavsar@bacancy.com>
2021-01-11 19:23:39 +0530

    changing the state of the variable

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,22 +2,26 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tag: ['tag1', 'tag2', 'tag3']
+
+    }
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
     }
     render() {
-        let classes = this.newMethod();
-
+        let classes = "btn m-2"
+        classes += this.state.count === 0 ? ' btn-primary ' : ' btn-warning '
         return (
             <>
                 <span className={classes}>{this.state.count}</span>
-                <button className="btn btn-dark"> click </button>
+                <button onClick={this.handleIncrement} className="btn btn-dark"> Increment </button>
+                <ul>
+                    {this.state.tag.map((Elem, index) => {
+                        return <li key={index}> {Elem} </li>
+                    })}
+                </ul>
             </>);
-    }
-
-    newMethod() {
-        let classes = "";
-        classes += this.state.count === 0 ? 'btn btn-primary' : 'btn btn-warning';
-        return classes;
     }
 }
 
